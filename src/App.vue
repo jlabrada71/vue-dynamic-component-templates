f804f7bee3fb8e06b8397037a0b1914012753b09
jlabrada <jlabrada@yahoo.com>
2023-02-15 18:09:59 -0300

    Showing sample components

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
   <HelloRenderFunction />
   <RenderFunctionExample msg="This is a test"></RenderFunctionExample>
   <JsxComponent />
+  <CompiledTemplate />
+  <CompiledTemplateWithRenderFunction />
   
 </template>
 <script setup>
@@ -10,6 +12,7 @@
   import DynamicTemplate from '@/components/DynamicTemplate.vue'
   import RenderFunctionExample from '@/components/RenderFunctionExample.js'
   import HelloRenderFunction from '@/components/HelloRenderFunction.js'
-  // import CompiledTemplate from '@/components/CompiledTemplate.js'
+  import CompiledTemplate from '@/components/CompiledTemplate.vue'
+  import CompiledTemplateWithRenderFunction from '@/components/CompiledTemplateWithRenderFunction.js'
 
 </script>
